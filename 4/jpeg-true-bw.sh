7c33db3b72d42193cf220c9b3a00225615675be2
Lukasz Gryglicki <lgryglicki@cncf.io>
2025-06-22 08:32:55 +0200

    Updates
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

--- 4/jpeg-true-bw.sh
+++ 4/jpeg-true-bw.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-R=1 G=1 B=1 LO=1 HI=1 O='.JPG:.png' PQ=1 jpegbw $*
+if [ -z "$LO" ]
+then
+  export LO='.5'
+fi
+if [ -z "$HI" ]
+then
+  export HI='.5'
+fi
+if [ -z "$Q" ]
+then
+  export Q='88'
+fi
+R=1 G=1 B=1 LO="$LO" HI="$HI" O='.JPG:.png' PQ=1 jpegbw $*
 for f in bw_*.png
 do
   b="${f%.png}"
   s="${b#bw_}"
-  magick "$f" -colorspace Gray -quality 90 "$b.jpg" && rm -f "$f" && exiftool -TagsFromFile "$s.JPG" -overwrite_original "$b.jpg"
+  magick "$f" -colorspace Gray -quality "$Q" "$b.jpg" && rm -f "$f" && exiftool -TagsFromFile "$s.JPG" -overwrite_original "$b.jpg"
 done
 
